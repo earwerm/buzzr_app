3cbd952a7dcef7cae60e3a7da2ecee6d26b7d585
earwerm <michael@earwerm.com>
2013-08-02 11:54:55 +1000

    user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  name       :string(255)
+#  email      :string(255)
+#
+
 class User < ActiveRecord::Base
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  name       :string(255)
+#  email      :string(255)
+#
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(:name => 'Bob', :email => 'bob@gmail.com')
+  end
+
+  it "should have a name" do
+    @user.name.should eq('Bob')
+  end
+
+  it "should have an email" do
+    @user.email.should eq('bob@gmail.com')
+  end
 end
+
