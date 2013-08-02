2c4ad4180eef1282e81de2f47ebb08f73f14038f
earwerm <michael@earwerm.com>
2013-08-02 11:27:19 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuzzrApp::Application.config.secret_key_base = '278c9e16301d9a216f7fc6df555afed0c0cbb44a3e92001d2b8d73a086840c41291c8c2de29081950f9a753b22367d3acb85bf21a743f654c52b6fcc0b24dd36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BuzzrApp::Application.config.session_store :cookie_store, key: '_buzzr_app_session'
