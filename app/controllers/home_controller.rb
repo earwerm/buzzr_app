dbfe7c4f64d33d7a1e5be5e486c04de549f7c53f
PACIFICA <MOOKBOOK13@Taruns-MacBook-Air.local>
2013-08-05 17:38:00 +1000

    instagram appended, map working, recenter working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,21 +2,16 @@ class HomeController < ApplicationController
   def index
   end
 
+# takes the form data from form partial on homepath
   def points
     locale = Location.create( :address => params[:locale] )
-
     lat = locale.latitude.to_f
     lng = locale.longitude.to_f
-
+# instagram api
     url = "https://api.instagram.com/v1/locations/search.json?lat=#{lat.round(3)}&lng=#{lng.round(3)}&client_id=efea46f4c52542348ced4c529263cf33"
-
+# creates an instance variable @result with the json object
     @result = HTTParty.get url
-
-
-    # @result.data.each do |locale|
-    #   Location.create(locale.latitude, locale.longitude)
-    # end
+# makes the json data available for ajax
     render :json => @result
-end
-
+  end
 end
