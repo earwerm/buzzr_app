340b06218fe721a2d91733b06f35ff32d49f7970
Pacifica <tarun@pacificasearch.com>
2013-08-08 23:48:43 +1000

    things working better

--- app/assets/javascripts/images_plot.js
+++ app/assets/javascripts/images_plot.js
@@ -1,33 +1,49 @@
 $(document).ready(function () {
-    // upon clicking the button with #addresses id, initialize the locale variable with the addresses value, run draw_points function;//
-    $('#address').click(function () {
-        console.log('address clickhandler');
-        alert('Getting info from Instagram, this may take a few seconds......');
-        var locale = $('#addresses').val();
-        console.log('locale='+locale);
-        draw_points(locale);
-    });
+        // upon clicking the button with #addresses id, initialize the locale variable with the addresses value, run draw_points function;//
+        $('#address').click(function () {
+                console.log('address clickhandler');
+                alert('Getting info from Instagram, this may take a few seconds......');
+                var locale = $('#addresses').val();
+                // locale = locale.replace(/[\|&;\$%@"<>\(\)\+,\, ]/g, "%20");
+                draw_points(locale);
+                console.log('locale=' + locale);
+                return false;
+                });
+         $('#address input').keydown(function(e) {
+            if (e.which === 13) {
+                $(this).trigger('click');
+                return false;
+            };
+            return false;
+        });
+
     var draw_points = function (locale) {
         $.ajax({
-            url: '/points/' + locale,
-            dataType: 'json',
+            url: '/points/' + locale
         }).done(function (results) {
+            // debugger;
             // iterates the add_marker function and parses the data from the json object to add_marker
             $.each(results.data, function (i, l) {
                 add_marker(l.latitude, l.longitude, l.name, l.id);
-                // makes all 4 elements available to dependent function
+                // makes all 4 elements available to listrender function;
                 listrender(l);
             });
             // centers map using first object lat and long
             center_map(results.data[0].latitude, results.data[0].longitude);
             // instagram sometimes returns errors, this alert makes the user aware;
-        }).error(function (results) {
-            alert("error! instagram says:" + results);
+        }).error(function (results, t, m) {
+            //alert("error! instagram says:" + results);
+
+                   if(t==="timeout") {
+                    alert("got timeout");
+                } else {
+                    alert(t);
+                }
         });
     };
 });
 
- // renders the marker info to image_list
+// renders the marker info to image_list
 var inforender = function (marker) {
     var id = marker.id; // Get the ID from marker
     console.log('The ID for this is really: ' + id);
@@ -43,7 +59,7 @@ var inforender = function (marker) {
         $('#images_list_modal').modal('hide')
         $('.modal img').attr('src', photo.data[0].images.low_resolution.url);
         $('.modal-footer p').text(photo.data[0].caption.text);
-        $('.modal-title h4').text(photo.data[0].location.name);
+        $('.modal-header h4').text(photo.data[0].location.name);
         $('#image_result').modal({
             keyboard: false
         });
--- app/assets/javascripts/responsivemap.js
+++ app/assets/javascripts/responsivemap.js
@@ -1,36 +1,36 @@
-google.maps.visualRefresh = true; //Optional
+// google.maps.visualRefresh = true; //Optional
 
-var respMap;
+// var respMap;
 
-function mymapini() {
-    var mapPos = new google.maps.LatLng(geoip_latitude(), geoip_longitude()); //Set the coordinates
-    var mapOpts = {
-    zoom: 18, //You can change this according your needs
-    disableDefaultUI: true, //Disabling UI Controls (Optional)
-    center: mapPos, //Center the map according coordinates
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+// function mymapini() {
+//     var mapPos = new google.maps.LatLng(geoip_latitude(), geoip_longitude()); //Set the coordinates
+//     var mapOpts = {
+//     zoom: 18, //You can change this according your needs
+//     disableDefaultUI: true, //Disabling UI Controls (Optional)
+//     center: mapPos, //Center the map according coordinates
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
 
-    respMap = new google.maps.Map(document.getElementById('mymap'),
-      mapOpts);
+//     respMap = new google.maps.Map(document.getElementById('map_canvass'),
+//       mapOpts);
 
-    var mapMarker = new google.maps.Marker({
-          position: mapPos,
-          map: respMap,
-          title: 'You can put any title'
-      });
+//     var mapMarker = new google.maps.Marker({
+//           position: mapPos,
+//           map: respMap,
+//           title: 'You can put any title'
+//       });
 
-    //This centers automatically to the marker even if you resize your window
-    google.maps.event.addListener(respMap, 'idle', function() {
-    window.setTimeout(function() {
-      respMap.panTo(mapPos.getPosition());
-    }, 250);
-  });
-}
+//     //This centers automatically to the marker even if you resize your window
+//     google.maps.event.addListener(respMap, 'idle', function() {
+//     window.setTimeout(function() {
+//       respMap.panTo(mapPos.getPosition());
+//     }, 250);
+//   });
+// }
 
-google.maps.event.addDomListener(window, 'load', mymapini);
+// google.maps.event.addDomListener(window, 'load', mymapini);
 
-$("#modalOpen").click(function(){ //Use it like <a href="#" id="modalOpen"...
-    $("#myModal").show(); //ID from the Modal <div id="myModal">...
-    google.maps.event.trigger(respMap, 'resize');
-});
+// // $("#modalOpen").click(function(){ //Use it like <a href="#" id="modalOpen"...
+// //     $("#myModal").show(); //ID from the Modal <div id="myModal">...
+//     google.maps.event.trigger(respMap, 'resize');
+// });
